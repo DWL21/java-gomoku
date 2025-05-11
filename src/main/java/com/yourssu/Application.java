61052b1eed0671cf1e7c6542b301ff8d19166512
dwl21 <nggus5@gmail.com>
2025-05-11 10:45:08 +0900

    feat: change example

--- src/main/java/com/yourssu/Application.java
+++ src/main/java/com/yourssu/Application.java
@@ -1,10 +1,10 @@
 package com.yourssu;
 
-import com.yourssu.controller.BoardGameController;
+import com.yourssu.controller.BoardGameControllerExample;
 
 public class Application {
     public static void main(String[] args) {
-        Runnable controller = new BoardGameController();
+        Runnable controller = new BoardGameControllerExample();
         controller.run();
     }
 }
